0d83012c896764a28bcd41667ebb29ca891b04c2
jordan.werre <47542013+jwerre1@users.noreply.github.com>
2019-03-21 22:53:51 -0400

    everything basically working, maybe some async problems

--- liri.js
+++ liri.js
@@ -2,6 +2,11 @@ require("dotenv").config();
 
 var keys = require("./keys.js");
 var moment = require('moment');
+var fs = require("fs");
+
+var Spotify = require('node-spotify-api');
+ 
+var spotify = new Spotify(keys.spotify);
 
 // var spotify = new Spotify(keys.spotify);
 
@@ -11,31 +16,41 @@ var command = process.argv[2];
 var processArgv = process.argv;
 var unique = "";
 
-for (var i = 3; i < processArgv.length - 1; i++) {
-  unique += (processArgv[i] + "+"); 
+unique = processArgv[3];
+
+for (var i = 4; i < processArgv.length; i++) {
+  unique += ("+" + processArgv[i]); 
 }
 
-unique += processArgv[processArgv.length - 1];
+
 
 console.log(unique);
 
-switch(command) {
-    case "concert-this":
-    concertThis();
-    break;
+basic();
 
-    case "spotify-this-song":
-    spotifyFunction();
-    break;
+function basic() {
+  
+  switch(command) {
+      case "concert-this":
+      concertThis();
+      break;
 
-    case "movie-this":
-    break;
+      case "spotify-this-song":
+      spotifyFunction();
+      break;
 
-    case "do-what-it-says":
-    break;
+      case "movie-this":
+      movieThis();
+      break;
+
+      case "do-what-it-says":
+      checkRandom();
+      break;
+  }
 }
 
 function concertThis() {
+  console.log(unique);
   var queryURL = "https://rest.bandsintown.com/artists/" + unique + "/events?app_id=codingbootcamp";
 
   axios.get(queryURL).then(
@@ -50,13 +65,14 @@ function concertThis() {
       }
     }
   );
-
 }
 
 function spotifyFunction() {
-    var Spotify = require('node-spotify-api');
- 
-    var spotify = new Spotify(keys.spotify);
+ console.log(unique);
+
+    if (unique === undefined) {
+      unique = "The+Sign+Ace+of+Base";
+  }
 
     spotify.search({ type: 'track', query: unique, limit: 1})
     .then(function(response) {
@@ -65,5 +81,61 @@ function spotifyFunction() {
         console.log("Song Name: " + response.tracks.items[0].name);
         console.log("Song Preview: " + response.tracks.items[0].preview_url);
         console.log("Album Name: " + response.tracks.items[0].album.name);
-    })
+    });
+  
+}
+
+function movieThis() {
+  if (unique === undefined) {
+    unique = "Mr.+Nobody";
+  }
+
+  var queryUrl = "http://www.omdbapi.com/?t=" + unique + "&y=&plot=short&apikey=trilogy";
+
+  axios.get(queryUrl).then(
+    function(response) {
+      // console.log(response);
+      console.log("* Title: " + response.data.Title);
+      console.log("* Year: " + response.data.Year);
+      console.log("* IMDB Rating: " + response.data.imdbRating);
+      console.log("* Rotten Tomatoes Rating: " + response.data.Ratings[1].Value);
+      console.log("* Countries: " + response.data.Country);
+      console.log("* Languages: " + response.data.Language);
+      console.log("* Plot: " + response.data.Plot);
+      console.log("* Actors: " + response.data.Actors);
+    }
+  );
+
+}
+
+
+function checkRandom() {
+fs.readFile("random.txt", "utf8", function(error, data) {
+
+  // If the code experiences any errors it will log the error to the console.
+  if (error) {
+    return console.log(error);
+  }
+
+  unique = "";
+  
+  var dataArr = data.split(",");
+
+  var dataArrInput = dataArr[1].split(" ")
+
+
+  unique = dataArrInput[0];
+
+  for (var j = 1; j < dataArrInput.length; j++) {
+    unique += ("+" + dataArrInput[j]); 
+  }
+
+
+  command = dataArr[0];
+
+  console.log(command);
+  console.log(unique);
+  basic();
+
+  });
 }
